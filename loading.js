169da7f70bd3174b75aac310a2abe4511dfa0ee8
AkbarMaliki <taufikakbarmalikitkj@gmali.com>
2025-07-06 00:32:14 +0800

    ipdate

---
+++ loading.js
@@ -0,0 +1,10 @@
+{
+    "apiKey": "AIzaSyDEbhZMnzuuIJ3JYPKAdQqrYN3s0sPT2fM",
+    "authDomain": "testing-54719.firebaseapp.com",
+    "databaseURL": "https://testing-54719-default-rtdb.firebaseio.com",
+    "projectId": "testing-54719",
+    "storageBucket": "testing-54719.appspot.com",
+    "messagingSenderId": "518827785832",
+    "appId": "1:518827785832:web:54d14cac7dd8c6987645dd",
+    "measurementId": "G-QZ1EHKMCTY"
+}
